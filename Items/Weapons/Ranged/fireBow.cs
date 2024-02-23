49c570e26e3e56cf64b1470829f53260821b3a33
Gbodson <gbodson02@gmail.com>
2024-02-23 16:56:15 -0500

    initial fire bow/arrow commit

---
+++ Items/Weapons/Ranged/fireBow.cs
@@ -0,0 +1,52 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.DataStructures;
+using Microsoft.Xna.Framework;
+using glacial_inferno.Projectiles;
+
+
+namespace glacial_inferno.Items.Weapons.Ranged
+{
+    public class fireBow : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.damage = 15;
+            Item.DamageType = DamageClass.Ranged;
+            Item.knockBack = 1.5f;
+            Item.shootSpeed = 10f;
+            Item.autoReuse = true;
+            Item.maxStack = 1;
+            Item.value = Item.sellPrice(0, 0, 80, 0);
+            Item.rare = ItemRarityID.Blue;
+            Item.noMelee = true;
+            Item.UseSound = SoundID.Item5;
+
+            Item.width = 10;
+            Item.height = 30;
+
+            Item.useTime = 25;
+            Item.useAnimation = 25;
+            Item.useStyle = ItemUseStyleID.Shoot;
+
+            //Change to custom projectile
+            Item.shoot = AmmoID.Arrow;
+            Item.useAmmo = AmmoID.Arrow;
+
+        }
+
+        //turns the arrows into fire arrows
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+        {
+            type = ModContent.ProjectileType<Projectiles.fireArrow>();
+        }
+
+        //Need to think about this needs to be somewhat expensive
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.Register();
+        }
+    }
+}
--- obj/Release/net6.0/glacial_inferno.AssemblyInfo.cs
+++ obj/Release/net6.0/glacial_inferno.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("glacial_inferno")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b0dcc5d409723d350233709aec578f87993e4c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("glacial_inferno")]
