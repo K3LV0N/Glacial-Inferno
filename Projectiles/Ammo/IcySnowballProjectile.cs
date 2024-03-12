3cfae0bba1b2373322a22ee117f50456bfe1a76e
Ryan Carroll <ryan.carroll421@gmail.com>
2024-03-12 17:29:43 -0400

    Added textures for IcySnowball

--- Projectiles/Ammo/IcySnowballProjectile.cs
+++ Projectiles/Ammo/IcySnowballProjectile.cs
@@ -3,6 +3,7 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 using Microsoft.Xna.Framework;
+using glacial_inferno.Buffs;
 
 namespace glacial_inferno.Projectiles.Ammo
 {
@@ -24,6 +25,7 @@ namespace glacial_inferno.Projectiles.Ammo
 		public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
 		{
 			base.OnHitNPC(target, hit, damageDone);
+            // LUKE KISSLING MADE THIS BUFF NOT ME -RYAN
 			target.AddBuff(ModContent.BuffType<Frozen>(), 600);
 		}
 	}
