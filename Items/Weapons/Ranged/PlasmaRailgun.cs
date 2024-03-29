f5d1a72fb5e4cfaaaba2dd72b26e10ebc78cfd06
Luke-Kissling <luke.kissling@gmail.com>
2024-03-29 16:14:46 -0400

    began working on a railgun

---
+++ Items/Weapons/Ranged/PlasmaRailgun.cs
@@ -0,0 +1,41 @@
+﻿using glacial_inferno.Projectiles.Weapons.Ranged;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace glacial_inferno.Items.Weapons.Ranged
+{
+    public class PlasmaRailgun : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.damage = 80;
+
+            Item.width = 70;
+            Item.height = 40;
+            Item.useTime = 28;
+            Item.useAnimation = 28;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.noMelee = true;
+            Item.knockBack = 8;
+            Item.value = Item.sellPrice(0, 10, 0, 0); // Adjust the sell price as needed
+            Item.mana = 20;
+            Item.rare = ItemRarityID.Pink;
+            Item.UseSound = SoundID.Item11;
+            Item.autoReuse = true;
+            Item.shoot = ModContent.ProjectileType<PlasmaRailgunProj>(); // Change this to the projectile type you want
+            Item.shootSpeed = 50f;
+           
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe r1 = CreateRecipe();
+            r1.AddIngredient(ItemID.TitaniumBar, 10);
+            r1.AddIngredient(ItemID.HellstoneBar, 10);
+            r1.AddTile(TileID.MythrilAnvil);
+            r1.Register();
+        }
+    }
+}
---
+++ Projectiles/Weapons/Ranged/PlasmaRailgunProj.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace glacial_inferno.Projectiles.Weapons.Ranged
+{
+    public class PlasmaRailgunProj : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.width = 10; // Width of the Projectile hitbox
+            Projectile.height = 6; // Height of the Projectile hitbox
+            Projectile.aiStyle = 1; // Use the default AI style (straight trajectory)
+            Projectile.friendly = true; // Friendly Projectile (doesn't harm players)
+    
+            Projectile.timeLeft = 600; // Time the Projectile exists (in frames)
+            
+            Projectile.ignoreWater = true; // Ignores water collision
+            Projectile.penetrate = 5;
+            AIType = ProjectileID.Bullet; // Use the Bullet AI type
+        }
+    
+    }
+
+}
