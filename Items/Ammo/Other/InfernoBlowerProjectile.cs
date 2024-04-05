dbaaeee2eb7a80a165c3758b1781349cc4fe859c
Maksim <maksim.peter777@gmail.com>
2024-04-05 17:29:25 -0400

    Add Inferno Blower

---
+++ Items/Ammo/Other/InfernoBlowerProjectile.cs
@@ -0,0 +1,86 @@
+using System;
+using Microsoft.Xna.Framework;
+using ReLogic.Utilities;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace glacial_inferno.Items.Ammo.Other
+{
+    public class InfernoBlowerProjectile : ModProjectile
+    {
+
+        public override void SetDefaults()
+        {
+            // Set the Projectile's width and height
+            Projectile.width = 10;
+            Projectile.height = 100;
+
+            // Make the Projectile invisible
+            Projectile.alpha = 255;
+            //Projectile.scale = 10f;
+            // Set the Projectile to fly straight
+            Projectile.aiStyle = 0;
+
+            // Other Projectile settings
+            Projectile.friendly = true; // Can damage enemies
+            Projectile.hostile = false; // Won't damage the player
+            // Projectile.range = true; // This is a ranged Projectile
+            Projectile.penetrate = -1; // Infinite penetration (doesn't disappear after hitting an enemy)
+            Projectile.timeLeft = 45; // Lasts for 10 seconds (60 frames per second * 10)
+            Projectile.ignoreWater = true; // Will not be slowed by water
+            Projectile.tileCollide = false; // Will collide with tiles (walls, ground, etc.)
+
+        }
+
+        //overide AI to spawn snow particles
+        public override void AI()
+        {
+
+            // Vector2 pos = Projectile.position; // Get the Projectile's position
+            // //move position down
+            // pos.Y += 30;
+            // //move position in front of player (pos or negative depending on direction player is facing)
+            // pos.X += 40 * Projectile.direction;
+
+
+            Vector2 pos = Projectile.position; // Get the Projectile's position
+
+            // Calculate the offset based on the player's aiming angle
+            float offset = 30f; // Set the initial offset distance
+            float angle = (float)Math.Atan2(Projectile.velocity.Y, Projectile.velocity.X); // Calculate the aiming angle
+            pos += new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle)) * offset; // Apply the offset based on the aiming angle
+            pos.Y += 10;
+            
+            int dustIndex = Dust.NewDust(pos, Projectile.width, 56, DustID.YellowTorch, Projectile.velocity.X, Projectile.velocity.Y, 100, Color.LightGoldenrodYellow, 1.5f);
+            Dust dust = Main.dust[dustIndex];
+
+            //dust.velocity *= 0.3f; // Reduce the velocity of the dust particle
+            dust.noGravity = true; // Make the dust particle not be affected by gravity
+
+        }
+
+
+        // If you need the Projectile to do something when it hits something, use this method
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+
+            //heal the target for damageDone
+            target.life += damageDone;
+
+            //on Hit apply the FrostBurn debuff to the enemy and "blow them back"
+            target.AddBuff(BuffID.Warmth, 60 * 5); // Apply the FrostBurn debuff for 5 seconds\
+            // Blow the enemy back by gradually decreasing speed in the same direction the Projectile is moving
+            //get actual velocity from x and y
+            float actual_vel = (float)System.Math.Sqrt(Projectile.velocity.X * Projectile.velocity.X + Projectile.velocity.Y * Projectile.velocity.Y);
+
+
+            float maxSpeedX = 10f; // Set the maximum speed ceiling
+            float maxSpeedY = 10f; // Set the maximum speed ceiling
+
+            target.velocity.X = MathHelper.Clamp(target.velocity.X + (5 * (Projectile.velocity.X / actual_vel)), -maxSpeedX, maxSpeedX);
+            target.velocity.Y = MathHelper.Clamp(target.velocity.Y + (8 * (Projectile.velocity.Y / actual_vel)), -maxSpeedY, maxSpeedY);
+        }
+
+    }
+}
---
+++ Items/Weapons/Ranged/InfernoBlower.cs
@@ -0,0 +1,99 @@
+using Terraria.DataStructures;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using glacial_inferno.Items.Ammo.Other;
+using System;
+
+namespace glacial_inferno.Items.Weapons.Ranged
+{
+    public class InfernoBlower : ModItem
+    {
+        private double manaAccumulator = 0;
+        //A weapon that "blows" bits of Hot Air towards the enemy.
+        //Shoots very many small projectiles that simulate wind
+        //when projectiles hit they Warmth the enemy and "blow them back" 
+        //uses very small amount of mana as ammo
+
+        public override void SetDefaults()
+        {
+            Item.damage = 1;
+            Item.useTime = 5;
+            Item.useAnimation = 5;
+            Item.knockBack = 0f;
+            Item.noMelee = true;
+            Item.notAmmo = true;
+            Item.DamageType = DamageClass.Magic;
+            //Item.useAmmo = AmmoID.None;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.value = 20000;
+            //Item.UseSound = SoundID.Item11;
+            Item.autoReuse = true;
+            Item.width = 25;
+            Item.height = 18;
+            
+
+
+
+
+
+
+
+            //dont matter so set to random values
+            Item.shoot = ProjectileID.SnowBallFriendly; // Set this to the ID of the projectile you want to shoot
+            Item.shootSpeed = 10f; // How fast the projectile is shot
+
+
+        }
+
+        //move the weapon to the player's hand
+        public override Vector2? HoldoutOffset()
+        {
+            return new Vector2(5, 0);
+        }
+
+        //on use
+        public override bool CanUseItem(Player player)
+        {
+            //if the player has no mana, return false
+            if (player.statMana == 0)
+            {
+                return false;
+            }
+            //otherwise take some mana and return true
+            manaAccumulator += 0.1;
+            if (manaAccumulator >= 1)
+            {
+                player.statMana -= 1;
+                manaAccumulator = 0;
+            }
+            return true;
+        }
+
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            
+            // Calculate the rotation to match the direction of the velocity
+            float rotation = (float)Math.Atan2(velocity.Y, velocity.X);
+
+            // Create the projectile at the specified position with the calculated rotation
+            Projectile projectile = Main.projectile[Projectile.NewProjectile(source, position, velocity, ModContent.ProjectileType<InfernoBlowerProjectile>(), damage, knockback, player.whoAmI)];
+
+            // Set the rotation of the projectile
+            projectile.rotation = rotation;
+
+            // Make sure the sprite faces the right way if it has a direction
+            if (projectile.spriteDirection == -1)
+            {
+                projectile.rotation += MathHelper.Pi;
+            }
+
+            // Return false because we don't want Terraria to shoot the projectile automatically
+            return false;
+        }
+
+
+    }
+}
