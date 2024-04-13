d737cd2c29313a5e5fad5bd572e08fe3f82e5a50
Conan Lee <158228339+colee69@users.noreply.github.com>
2024-04-12 20:45:18 -0400

    EndlessSnowBallPouch
    Infinite snow balls

---
+++ Items/Ammo/Bullets/EndlessSnowBallPouch.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+using glacial_inferno.Items.Weapons.Projectile;
+
+namespace glacial_inferno.Items.Weapons.Ammo
+{
+    internal class EndlessSnowBallPouch : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 8;
+            Item.height = 8;
+            Item.ammo = AmmoID.Snowball;
+            Item.maxStack = 1;
+            Item.consumable = false;
+            Item.damage = 10;
+            Item.knockBack = 1.5f;
+            Item.shoot = ProjectileID.SnowBallFriendly;
+            Item.shootSpeed = 10;
+            Item.value = 30;
+        }
+        
+        public override void AddRecipes()
+        {
+            Recipe r1 = CreateRecipe();
+            r1.AddIngredient(ItemID.Snowball, 3996);
+            r1.AddTile(TileID.WorkBenches);
+            r1.Register();
+        }
+
+    }
+}
